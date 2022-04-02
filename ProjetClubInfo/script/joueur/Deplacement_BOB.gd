727ad4c4a651a235fd112a34966dbe11a83813db
Jules <102732657+JulesChe@users.noreply.github.com>
2022-04-03 00:39:23 +0200

    Ajout d'un ennemie + Ajout de shoot
    L'ennemie a un soucis de collision avec le vide quand il arrive au bout de la plateforme. Il ne peut pas encore faire de dégâts.  J'ai mis pas mal de temps à le faire a cause d'un pb cet enculux
    
    Pour le shoot j'ai implémenté les balles sans créer de dégâts.

--- ProjetClubInfo/script/joueur/Deplacement_BOB.gd
+++ ProjetClubInfo/script/joueur/Deplacement_BOB.gd
@@ -3,7 +3,12 @@ extends KinematicBody2D
 export var speed = 300
 export var jump_speed = 700
 export var gravity = 30
+export var balleVit = 1000
+export var balleRate = 0.2
 
+
+var balle = preload("res://Scenes/joueur/balle.tscn")
+var peutTirer = true
 var velocity = Vector2()
 
 func _physics_process(_delta):
@@ -13,4 +18,17 @@ func _physics_process(_delta):
 	velocity = move_and_slide(velocity, Vector2(0, -1))
 	if is_on_floor() and Input.is_action_just_pressed("jump") :
 		velocity.y = -jump_speed
-	
+
+	# Partie Shoot
+	if Input.is_action_pressed("shoot") and peutTirer == true:
+		var balleInstance = balle.instance()
+		balleInstance.position = get_global_position()
+		balleInstance.apply_impulse(Vector2(),Vector2(balleVit,0).rotated(rotation))
+		get_tree().get_root().add_child(balleInstance)
+		peutTirer = false
+		yield(get_tree().create_timer(balleRate), "timeout")
+		peutTirer = true
+		
+		
+		
+		
