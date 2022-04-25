0fd8c00cc4bc5b1f564e6cd8b8f735ad98091625
Jules <102732657+JulesChe@users.noreply.github.com>
2022-04-25 20:08:45 +0200

    Ajout d'un ennemi
    Il detecte le vide et les murs (pas de systeme de détection et game over)

---
+++ ProjetClubInfoV2/2DPlatformerEntrainement/Scenes/ennemi/ennemi.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+var velocity = Vector2(0,0)
+export var direction = -1
+export var detectCliff = true
+
+func _ready():
+	
+	if direction == 1:
+		$AnimatedSprite.flip_h = true
+	$floorChecker.position.x = $CollisionShape2D.shape.get_extents().x * direction
+	$floorChecker.enabled = detectCliff
+
+func _physics_process(delta):
+	
+	if is_on_wall() or not $floorChecker.is_colliding() and detectCliff and is_on_floor():
+		direction = direction * -1
+		$AnimatedSprite.flip_h = not $AnimatedSprite.flip_h
+		$floorChecker.position.x = $CollisionShape2D.shape.get_extents().x * direction
+	
+	velocity.y += 20
+	
+	velocity.x = 50 * direction
+	
+	velocity = move_and_slide(velocity,Vector2.UP)
