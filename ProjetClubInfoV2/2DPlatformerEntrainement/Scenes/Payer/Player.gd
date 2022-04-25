ca8bd62dce950bb965e758e7bee093307124f075
CroustiBapt <89194028+CobsiHD@users.noreply.github.com>
2022-04-25 17:54:36 +0200

    evolution niveau

--- ProjetClubInfoV2/2DPlatformerEntrainement/Scenes/Payer/Player.gd
+++ ProjetClubInfoV2/2DPlatformerEntrainement/Scenes/Payer/Player.gd
@@ -25,7 +25,7 @@ func _physics_process(_delta):
 	
 	velocity.y += gravity
 
-	if Input.is_action_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMPFORCE
 	
 	velocity = move_and_slide(velocity,Vector2.UP)
