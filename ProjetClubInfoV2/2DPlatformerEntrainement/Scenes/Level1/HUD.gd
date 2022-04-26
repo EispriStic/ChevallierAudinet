690323265b3dd8949f55abd362b09b1e491eec31
Jules <102732657+JulesChe@users.noreply.github.com>
2022-04-26 19:11:56 +0200

    Ajout de yeux qui se ferment et police

--- ProjetClubInfoV2/2DPlatformerEntrainement/Scenes/Level1/HUD.gd
+++ ProjetClubInfoV2/2DPlatformerEntrainement/Scenes/Level1/HUD.gd
@@ -4,6 +4,7 @@ var coins = 0
 
 func _ready():
 	$COINS.text = String(coins)
+	$Sprite.hide()
 
 func _on_coin_collected():
 	coins += 1
@@ -12,3 +13,6 @@ func _on_coin_collected():
 	if coins == 4:
 		get_tree().change_scene("res://Scenes/Gagne/Gagne.tscn")
 
+	if coins == 3 :
+		$Sprite.show()
+		$Sprite.play("eyes")
